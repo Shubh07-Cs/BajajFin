ab7e3dce7934fc84135fc2d565b3d5f484cbff16
Shubh07-Cs <srai21029@gmail.com>
2025-08-08 23:12:06 +0530

    added health funtion for health checkup3

--- app/main.py
+++ app/main.py
@@ -43,3 +43,11 @@ async def root():
 @app.get("/health")
 def healthcheck():
     return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
