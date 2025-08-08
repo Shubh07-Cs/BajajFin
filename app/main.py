fc0a837b1de99e80b7fdde544f10db785a5bc618
Shubh07-Cs <srai21029@gmail.com>
2025-08-08 21:32:25 +0530

    added health funtion for health checkup2

--- app/main.py
+++ app/main.py
@@ -31,12 +31,17 @@ app.include_router(api_router, prefix="/api/v1", tags=["queries"])
 # Initialize your core query service
 query_service = QueryService()
 
-
 @app.get("/")
-async def root():
-    return {"message": "Welcome! Use POST /api/v1/hackrx/run to submit queries."}
-
-
-@app.get("/health")
-def healthcheck():
+def root():
     return {"status": "ok"}
+
+
+
+# @app.get("/")
+# async def root():
+#     return {"message": "Welcome! Use POST /api/v1/hackrx/run to submit queries."}
+
+
+# @app.get("/health")
+# def healthcheck():
+#     return {"status": "ok"}
